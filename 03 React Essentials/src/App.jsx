9a8180fc1892deddf7047083a410ab920ac6f419
Ifti-Alamin <iftialmin10@gmail.com>
2024-03-27 13:30:52 +0600

    Folder 03 file 18

--- 03 React Essentials/src/App.jsx
+++ 03 React Essentials/src/App.jsx
@@ -1,38 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
-import componentsImg from "./assets/components.png";
-
-const reactDescription = ["Fundamental", "Core", "Crucial"];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-// React component
-function Header() {
-  const description = reactDescription[getRandomInt(2)];
-
-  // JSX
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import { CORE_CONCEPTS } from "./data.js"; // "" remove all extra space or we will get error
+import Header from "./components/Header.jsx"; // we named in capital letter bcz we use it like a tag
+import CoreConcept from "./components/CoreConcept.jsx";
 
 function App() {
   return (
@@ -43,13 +11,13 @@ function App() {
           <h2> Core Concepts</h2>
           <ul>
             <CoreConcept
-              tile="Components"
-              description="The core UI building block."
-              image={componentsImg}
+              tile={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <h2>Time to get started!</h2>
---
+++ 03 React Essentials/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import reactImg from "../assets/react-core-concepts.png"; // .. two dir up
+
+const reactDescription = ["Fundamental", "Core", "Crucial"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+// React component
+export default function Header() {
+  const description = reactDescription[getRandomInt(2)];
+
+  // JSX
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
